2ca4f2990d6c4148822625c65c6b02dcaef769aa
melissatanja22 <105611965+melissatanja22@users.noreply.github.com>
2024-01-03 12:57:17 -0500

    updated

--- feathers3.js
+++ feathers3.js
@@ -138,27 +138,27 @@ class Form {
 
 		if(this.ax < 0 && this.bx < 0){
 
-			this.ax = width;
+			this.ax = width/2;
 			this.bx = this.ax + 25;
 
 		}
 
 		if(this.ay < 0 && this.by < 0){
 
-			this.ay = height;
+			this.ay = height/2;
 			this.by = this.ay + 25;
 
 		}
 
 		if(this.ax > width && this.bx > width){
 
-			this.ax = 0;
+			this.ax = width/4;
 			this.bx = this.ax + 25;
 		}
 
 		if(this.ay > height && this.by > width){
 
-			this.ay = 0;
+			this.ay = height/4;
 			this.by = this.ay + 25;
 
 		}
